f375b3d56cae6ef5404dc2172aa3ad677fb9fe52
Bernard-Otieno <bernard.otieno@strathmore.edu>
2023-10-17 14:56:38 +0300

    form

--- resources/views/deposit.blade.php
+++ resources/views/deposit.blade.php
@@ -3,35 +3,7 @@
 @section('content')
 <form action="#" class="ms-auto me-auto mt-3" style="width: 500px;">
 @csrf
-            <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Account</span>
-            <input type="text" class="form-control" placeholder="Account_no">
-
-            <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Your Email">
-            <span class="input-group-text" id="basic-addon2">@example.com</span>
-            </div>
-
            
-            <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-            <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-            </div>
-
-            <div class="input-group mb-3">
-            <span class="input-group-text" >$</span>
-            <input type="text" class="form-control"placeholder="Amount" >
-            <span class="input-group-text">.00</span>
-            </div>
-
-            <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="card_no">
-            <span class="input-group-text">@</span>
-            <input type="text" class="form-control" placeholder="CVV">
-            </div>
-
-            <div class="input-group">
-            <span class="input-group-text">With textarea</span>
-            <textarea class="form-control" aria-label="With textarea"></textarea>
-            </div>
+           
 </form>
 @endsection
--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -1,7 +1,19 @@
 @extends('layout') 
 @section('title','Transactions')
 @section('content')
-<h1>HELLO
-</h1>
+<h1 class="ms-auto me-auto mt-3" style="width:500px;">Transaction Form</h1>
+<form action="#" class="ms-auto me-auto mt-3" style="width: 500px;">
+
+        <label for="sender">Sender's Account number:</label>
+        <input type="text" id="sender" name="sender" required><br><br>
+
+        <label for="recipient">Recipient's Account number:</label>
+        <input type="text" id="recipient" name="recipient" required><br><br>
+
+        <label for="amount">Amount:</label>
+        <input type="number" id="amount" name="amount" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
    
 @endsection
